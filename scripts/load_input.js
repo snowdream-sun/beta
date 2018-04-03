2f84d98053cea50c1c984ccea43fa6501f6ad011
snowdream-sun <33110024+snowdream-sun@users.noreply.github.com>
2018-04-03 21:09:34 +0800

    Update load_input.js

--- scripts/load_input.js
+++ scripts/load_input.js
@@ -2,21 +2,21 @@
 
 // Functions
 function loadEgI() {
-    location = "page.html";
     sessionStorage.setItem("userInput", JSON.stringify(EG_MOORE_1));
     sessionStorage.setItem("fsmName", "Example: Moore #1");
+    location = "page.html";
 }
 
 function loadEgII() {
-    location = "page.html";
     sessionStorage.setItem("userInput", JSON.stringify(EG_MOORE_2));
     sessionStorage.setItem("fsmName", "Example: Moore #2");
+    location = "page.html";
 }
 
 function loadEgIII() {
-    location = "page.html";
     sessionStorage.setItem("userInput", JSON.stringify(EG_MEALY));
     sessionStorage.setItem("fsmName", "Example: Mealy");
+    location = "page.html";
 }
 
 function toEnterInput() {
